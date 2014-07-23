f02758ae9937c34a3be38e3d0943e085cdb883a9
Kalpit gajera <kalpitgajera3@gmail.com>
2014-07-23 18:33:44 +0530

    Update AppDelegate.h

--- SearchDisplay Controller/AppDelegate.h
+++ SearchDisplay Controller/AppDelegate.h
@@ -1,24 +1,55 @@
 //
-//  AppDelegate.h
-//  SearchDisplay Controller
+//  AppDelegate.m
+//  rtfToPdf
 //
-//  Created by NLS17 on 18/07/14.
-//  Copyright (c) 2014 Kalpit Gajera. All rights reserved.
+//  Created by NLS17 on 23/07/14.
+//
+//
+
+
+//
+//  AppDelegate.h
+//  rtfToPdf
+//
+//  Created by NLS17 on 23/07/14.
+//
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreText/CoreText.h>
+#define kBorderInset            20.0
+#define kBorderWidth            1.0
+#define kMarginInset            10.0
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
+{
+    CGSize pageSize;
+    NSString *pdfFileName;
+    NSString *contents_for_pdf;
+}
 
 @property (strong, nonatomic) UIWindow *window;
+- (void)generatePdfWithFilePath:(NSString *)thefilePath;
 
 @end
 
 
-//pdf create from rtf
 
 
-NSString *rtf_path = [[NSBundle mainBundle] pathForResource:@"Example" ofType:@"rtf"];
+
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
+    NSString *rtf_path = [[NSBundle mainBundle] pathForResource:@"Example" ofType:@"rtf"];
     contents_for_pdf = [[NSString alloc] initWithContentsOfFile:rtf_path encoding:NSUTF8StringEncoding error:nil];
     NSLog(@"%@",contents_for_pdf);
     contents_for_pdf = [contents_for_pdf stringByReplacingOccurrencesOfString:@"I am new to StackOverflow" withString:@"I regularly visit StackOverflow"];
@@ -84,3 +115,32 @@ NSString *rtf_path = [[NSBundle mainBundle] pathForResource:@"Example" ofType:@"
     UIAlertView *Alert = [[UIAlertView alloc] initWithTitle:@"PDF Created" message:@"Sucessfull" delegate:self cancelButtonTitle:@"ok" otherButtonTitles:nil, nil];
     [Alert show];
 }
+
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+@end
